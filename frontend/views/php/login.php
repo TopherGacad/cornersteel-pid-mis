1043dacd1314ea3a0d8a9958157cb3121e59c9d8
TopherGacad <Gacadtophs@gmail.com>
2023-07-07 13:53:45 +0800

    change the browser title

--- frontend/views/php/login.php
+++ frontend/views/php/login.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>PID - Login</title>
     
     <!-- STYLESHEET -->
     <link rel="stylesheet" href="../../public/css/login.css">
